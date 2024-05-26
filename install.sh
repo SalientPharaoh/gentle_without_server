67486240e346540911dbea0625b96bc5c88e94f0
Sahil Khirwal <sahilkhirwal1012@gmail.com>
2024-05-27 04:23:06 +0530

    adding permissions

--- install.sh
+++ install.sh
@@ -7,14 +7,19 @@ git submodule update
 
 ./install_deps.sh
 echo "Dependencies installed."
+
 echo "Installing OpenFst..."
+chmod +x install_openFst.sh
 ./install_openFst.sh
 echo "OpenFst installed."
+
 echo "Installing Kaldi..."
 (cd ext && ./install_kaldi.sh)
 echo "Kaldi installed."
+
 echo "Installing models..."
 ./install_models.sh
 echo "Models installed."
+
 cd ext && make depend && make
 echo "Gentle is ready to use."
